42069be27d9ee42ac05123d69bf6d27989c75f13
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2023-11-27 13:19:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ for path in paths:
 		html_path.append(path)
 verbose = False
 tissue = True
-output_name = 'output/Results.csv'
+output_name = 'output/Glioblastoma.csv'
 
 firefox_options = Options()
 firefox_options.add_argument('--headless')
@@ -243,63 +243,68 @@ for page in html_path:
 		
 data_for_studies = {}
 
-#codes = ['GSE244310']
+#codes = ['GSE48865']
 
 #Parse html pages for accession codes	
 n_studies = 0
 
-		
-for value in codes:
-#	if n_studies == 20: #Limits the number of experiments parsed
-#		break
-	geo_path = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + value
-	html_page = requests.get(geo_path)
-	data_for_studies[value] = {}
-	data_for_studies[value]['Link'] = geo_path	
-	if html_page.status_code==200:
-		html_content=html_page.text
-		print('Parsing ' + value + '... \n')
-		with open('tmp/Page.html','w',encoding='utf-8') as file:
-			file.write(html_content)
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['Experiment_Type'] =  experimentTyper(texto)
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['Platform'] = platformFinder(texto)
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['Organism'] = organismFinder(texto)
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['Samples'] = sampleFinder(texto)
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['SRA'] = sraChecker(texto)
-			
-		with open('tmp/Page.html','r') as texto:
-			if data_for_studies[value]['SRA'] == 'Yes':
-				data_for_studies[value]['SRA_link'] = sralinkFinder(texto)
-			else:
-				data_for_studies[value]['SRA_link'] = 'NA'
-		if tissue == True: 
-			data_for_studies[value]['Tissue'], data_for_studies[value]['Cells'] = tissueFinder()		
-		else:
-			data_for_studies[value]['Tissue'] = ''
-			data_for_studies[value]['Cells'] = ''
-	
-		with open('tmp/Page.html','r') as texto:
-			data_for_studies[value]['Title'] = getTitle(texto)	
-		n_studies += 1		
-		seconds = time() - start
-		tax = (seconds/n_studies*len(codes)) - seconds 
-		print('\nDone (' + str(n_studies) + '/' + str(len(codes))+ ') - (Approximately ' + str(round(tax)) + ' seconds remaining) \n---------\n\n')
-	else:
-		print(f'Failed to download HTML. Status code: {html_page.status_code}')
-	
-		
 with open(output_name,'w') as texto:
 	texto.write('Accession code ; Link ; Experiment Type ; Platform ; Organism ; Samples ; SRA ; SRA Link ; Tissue ; Cell type ; Title \n')
-	for key, value in data_for_studies.items():
-		if 'Experiment_Type' in value.keys():
-			texto.write(key + ' ; ' + value['Link'] + ' ; ' + value['Experiment_Type'] + ' ; ' + value['Platform'] + ' ; ' + value['Organism'] + ' ; ' + value['Samples'] + ' ; ' + value['SRA'] + ' ; ' + value['SRA_link'] + ' ; ' + value['Tissue']  + ' ; ' + value['Cells'] + ' ; ' + value['Title'] + '\n')
-		
-		
+
+with open(output_name,'a') as output:		
+	for value in codes:
+	#	if n_studies == 20: #Limits the number of experiments parsed
+	#		break
+		geo_path = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + value
+		html_page = requests.get(geo_path)
+		data_for_studies[value] = {}
+		data_for_studies[value]['Link'] = geo_path
+		output.write(value + ' ; ')
+		output.write(geo_path + ' ; ')	
+		if html_page.status_code==200:
+			html_content=html_page.text
+			print('Parsing ' + value + '... \n')
+			with open('tmp/Page.html','w',encoding='utf-8') as file:
+				file.write(html_content)
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['Experiment_Type'] =  experimentTyper(texto)
+				output.write(data_for_studies[value]['Experiment_Type'] + ' ; ')
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['Platform'] = platformFinder(texto)
+				output.write(data_for_studies[value]['Platform'] + ' ; ')
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['Organism'] = organismFinder(texto)
+				output.write(data_for_studies[value]['Organism'] + ' ; ')
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['Samples'] = sampleFinder(texto)
+				output.write(data_for_studies[value]['Samples'] + ' ; ')
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['SRA'] = sraChecker(texto)
+				output.write(data_for_studies[value]['SRA'] + ' ; ')
+				
+			with open('tmp/Page.html','r') as texto:
+				if data_for_studies[value]['SRA'] == 'Yes':
+					data_for_studies[value]['SRA_link'] = sralinkFinder(texto)
+				else:
+					data_for_studies[value]['SRA_link'] = 'NA'
+				output.write(data_for_studies[value]['SRA_link'] + ' ; ')	
+			if tissue == True: 
+				data_for_studies[value]['Tissue'], data_for_studies[value]['Cells'] = tissueFinder()
+			else:
+				data_for_studies[value]['Tissue'] = ''
+				data_for_studies[value]['Cells'] = ''
+			output.write(data_for_studies[value]['Tissue'] + ' ; ' + data_for_studies[value]['Cells'] + ' ; ')			
+			with open('tmp/Page.html','r') as texto:
+				data_for_studies[value]['Title'] = getTitle(texto)
+			output.write(data_for_studies[value]['Title'] + ' \n ')				
+			n_studies += 1		
+			seconds = time() - start
+			tax = (seconds/n_studies*len(codes)) - seconds 
+			print('\nDone (' + str(n_studies) + '/' + str(len(codes))+ ') - (Approximately ' + str(round(tax)) + ' seconds remaining) \n---------\n\n')
+		else:
+			print(f'Failed to download HTML. Status code: {html_page.status_code}')
+			output.write('Error \n')
+		output.flush()	
 	
 	
 	
