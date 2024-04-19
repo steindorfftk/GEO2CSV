bfd421e5b9fa6505618b07e638b010e9ca753b2d
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2024-04-19 17:28:25 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - Download the program files: Click on '<> Code' and then on 'Download ZIP'
 - Place the 'GEO-scraper_main.zip' file on the installation directory of your choice.
 - Chang into the installation directory and unzip the file (unzip GEO-scraper_main.zip)  
-- Install necessary packages: selenium, beutifulsoup4 and requests (pip install package)
+- Install necessary packages: selenium, beutifulsoup4, requests and lxml (pip install package)
 
 # Input File Preparation
 - This program is suitable to scrape data from any list of studies found after a Gene Expression Omnibus (GEO) search. The input file must be one or more html files with search results. Prepare the file as following:
@@ -14,11 +14,14 @@
 
 # GEO_scraper Utilization
 - 1 - Add the GEO search results html files in the input directory (GEO_scraper-main/input)
-- 2 - In-script configuration: there are 3 variables you can define in-script - verbose, tissue and output_name.
-- 2.1 - verbose: set to 'True' if you want the terminal to print the information while you are scraping, either else keep on 'False'. This doesn't alter the execution time or the final output.
-- 2.2 - complete: set to 'True' if you want the program to gather information on Tissue type and Cell type from the studies. This considerably increases the execution time but it's recommended.
-- 2.3 - output_name: set the desired output file name. The standard name is 'output'.
-- 3 - Run the main.py file (python3 main.py)
-- 4 - A .csv file with basic information about these studies will be created in output folder
+- 2 - Run the main.py file (python3 main.py)
+- 3 - A .csv file with basic information about these studies will be created in output folder
 
-## This version retrieves the following information: Accession code, GEO link, Citation, Experiment type, Platform, Organisms, Number of samples, SRA availability, SRA link, Tissues, Cell Types, Cell Lines and Title
+## GEO_scraper Options
+Currently, there are 3 options you can set when running the script - verbose, quick-mode and output name.
+- output name (-o): Use it to define the output name. Example: "-o My_data" will produce the My_data.csv output file. If not specified, the output file will be named 'output.csv'.
+- verbose (-v): Use it if you want the terminal to print the information while you are scraping. This doesn't alter the execution time or the final output.
+- quick-mode (-q): Use it if you want the program to run on quick mode, i.e., gather only basic information (doesnt collect data on Tissue type, Cell type and Race/Ethnicity/Ancestry from the study samples). This considerably reduces the execution time but excludes relevant study information.
+
+
+## This version retrieves the following information: Accession code, GEO link, Citation, Experiment type, Platform, Organisms, Number of samples, SRA availability, SRA link, Tissues, Cell Types, Cell Lines, Race/Ethnicity/Ancestry and Title
--- main.py
+++ main.py
@@ -9,25 +9,8 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 import re 
-
-print('Starting')
-
-#OPTIONS
-verbose = False
-complete = True
-output_name = 'output'
-
-#Input file
-paths = os.listdir('input/')
-
-html_path = []
-
-for path in paths:
-	if '.html' in path:
-		html_path.append(path)
-
-firefox_options = Options()
-firefox_options.add_argument('--headless')
+import argparse
+import datetime
 
 def getAccession(file):
 	accessionCodes = []
@@ -171,10 +154,13 @@ def citationFinder():
 		citation = 'Citation missing'
 	if verbose == True:
 		print('Citation: ' + citation)	
+	driver.quit()
+	citation = citation.replace(';',',')
 	return citation
 
-def tissueFinder():
+def sampledetailFinder():
 	samplecodes = []
+	ppp_tags = 'A'
 	tissues = []
 	driver = webdriver.Firefox(options=firefox_options)
 	driver.get(geo_path)
@@ -211,9 +197,27 @@ def tissueFinder():
 	tissues = []
 	cell_types = []
 	cell_lines = []
+	races = []
+	ethnicities = []
+	ancestries = []
+	ppp_tags = {}
+	if len(p_tags) > 0:
+		for value in p_tags:
+			ppp_tags[value] = {}
+			pat = f'{value}:(.*?)<br>'
+			patt = re.compile(pat, re.DOTALL)
+			ppp_tags[value]['pattern'] = patt
+			ppp_tags[value]['data'] = [] 
+			ppp_tags[value]['xdata'] = {}
+			ppp_tags[value]['string'] = ''
 	pattern_t = re.compile(r'tissue:(.*?)<br>', re.DOTALL)
 	pattern_c = re.compile(r'cell type:(.*?)<br>', re.DOTALL)
 	pattern_l = re.compile(r'cell line:(.*?)<br>', re.DOTALL)
+	pattern_r = re.compile(r'race:(.*?)<br>', re.DOTALL)
+	pattern_e1 = re.compile(r'reported_ethnicity:(.*?)<br>', re.DOTALL)
+	pattern_e2 = re.compile(r'<br>ethnicity:(.*?)<br>', re.DOTALL)
+	pattern_e3 = re.compile(r'race/ethnicity:(.*?)<br>', re.DOTALL)
+	pattern_a = re.compile(r'ancestry:(.*?)<br>', re.DOTALL)	
 	for valor in sample_links:
 		sample_path = valor
 		sample_html = requests.get(sample_path)
@@ -225,6 +229,9 @@ def tissueFinder():
 				tdata = {}
 				cdata = {}
 				ldata = {}
+				rdata = {}
+				edata = {}
+				adata = {}
 				for line in texto:
 					if 'tissue:' in line:
 						match = pattern_t.search(line)
@@ -241,6 +248,40 @@ def tissueFinder():
 						if match:
 							result=match.group(1).strip()
 							cell_lines.append(result)
+					if 'race:' in line:
+						match = pattern_r.search(line)
+						if match:
+							result=match.group(1).strip()
+							races.append(result)
+					if 'reported_ethnicity:' in line:
+						match = pattern_e1.search(line)
+						if match:
+							result=match.group(1).strip()
+							ethnicities.append(result)
+					if '<br>ethnicity:' in line:
+						match = pattern_e2.search(line)
+						if match:
+							result=match.group(1).strip()
+							ethnicities.append(result)
+					if 'race/ethnicity:' in line:
+						match = pattern_e3.search(line)
+						if match:
+							result=match.group(1).strip()
+							races.append(result)
+							ethnicities.append(result)
+					if 'ancestry:' in line:
+						match = pattern_a.search(line)
+						if match:
+							result=match.group(1).strip()
+							ancestries.append(result)
+					if len(p_tags) > 0:		
+						for value in p_tags:
+							if value in line:
+								match = ppp_tags[value]['pattern'].search(line)
+								if match:
+									result=match.group(1).strip()
+									ppp_tags[value]['data'].append(result)
+									
 				for value in tissues:
 					if value not in tdata.keys():
 						tdata[value] = tissues.count(value)
@@ -250,9 +291,35 @@ def tissueFinder():
 				for value in cell_lines:
 					if value not in ldata.keys():
 						ldata[value] = cell_lines.count(value)
+				for value in races:
+					if value not in rdata.keys():
+						rdata[value] = races.count(value)
+				for value in ethnicities:
+					if value not in edata.keys():
+						edata[value] = ethnicities.count(value)
+				for value in ancestries:
+					if value not in adata.keys():
+						adata[value] = ancestries.count(value)
+	if len(p_tags) > 0:
+		for value in p_tags:
+			for valueb in ppp_tags[value]['data']:
+				if valueb not in ppp_tags[value]['xdata'].keys():
+					ppp_tags[value]['xdata'][valueb] = ppp_tags[value]['data'].count(valueb)
 	tissue = ''
 	cells = ''
 	lines = ''
+	races1 = ''
+	ethnicities1 = ''
+	ancestries1 = ''
+	if len(p_tags) > 0:
+		for valor in p_tags:
+			for key, value in ppp_tags[valor]['xdata'].items():
+				if len(ppp_tags[valor]['xdata'].keys()) > 0:
+					keys_list = list(ppp_tags[valor]['xdata'].keys())
+					if key == keys_list[-1]:
+						ppp_tags[valor]['string'] += key + ' (' + str(value) + ')'
+					else:
+						ppp_tags[valor]['string'] += key + ' (' + str(value) + ') ' + ' / '
 	for key, value in tdata.items():
 		if len(tdata.keys()) > 0:
 			keys_list = list(tdata.keys())		
@@ -274,39 +341,103 @@ def tissueFinder():
 				lines += key + ' (' + str(value) + ')'
 			else:
 				lines += key + ' (' + str(value) + ') ' + ' / '	
+	for key, value in rdata.items():
+		if len(rdata.keys()) > 0:
+			keys_list = list(rdata.keys())	
+			if key == keys_list[-1]:
+				races1 += key + ' (' + str(value) + ')'
+			else:
+				races1 += key + ' (' + str(value) + ') ' + ' / '	
+	for key, value in edata.items():
+		if len(edata.keys()) > 0:
+			keys_list = list(edata.keys())	
+			if key == keys_list[-1]:
+				ethnicities1 += key + ' (' + str(value) + ')'
+			else:
+				ethnicities1 += key + ' (' + str(value) + ') ' + ' / '	
+	for key, value in adata.items():
+		if len(adata.keys()) > 0:
+			keys_list = list(adata.keys())	
+			if key == keys_list[-1]:
+				ancestries1 += key + ' (' + str(value) + ')'
+			else:
+				ancestries1 += key + ' (' + str(value) + ') ' + ' / '	
 	if verbose == True:
 		print('Tissues: ' + tissue)
 		print('Cell types: ' + cells)
-		print('Cell lines: ' + lines)	
-	return tissue, cells, lines		
-			
-		
-					
+		print('Cell lines: ' + lines)
+		print('Races: ' + races1)
+		print('Ethnicities: ' + ethnicities1)
+		print('Ancestries: ' + ancestries1)	
+	return tissue, cells, lines, races1 , ethnicities1 , ancestries1, ppp_tags	
 
+#Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true', help='Set verbose to True')
+parser.add_argument('-q', '--quick', action='store_true', help='Set complete to False')
+parser.add_argument('-o', '--output', type=str, default='output',help='Output file name')
+parser.add_argument('-t', '--tags', type=str, default='',help='Personalized tags')
+args = parser.parse_args()
+
+#Save personalized tags
+p_tags = args.tags.split()
+
+
+#Set options
+verbose = False
+if args.verbose:
+	verbose = True
+complete = True
+if args.quick:
+	complete = False
+output_name = args.output
+
+#Set input files
+paths = os.listdir('input/')
+html_path = []
+for path in paths:
+	if '.html' in path:
+		html_path.append(path)
+
+#Set selenium options
+firefox_options = Options()
+firefox_options.add_argument('--headless')
+
+#Start timer
 start = time()
 
 #Get accession codes from input file	
 codes = []
-
 for page in html_path:
 	path = 'input/' + page
 	acc = getAccession(path)
 	for value in acc:
 		codes.append(value)
 
-		
+#Start data dictionary		
 data_for_studies = {}
 
+#Define output path
 output_name = 'output/' + output_name + '.csv'
 
 #Parse html pages for accession codes	
 n_studies = 0
+all_codes = 0
 
+#Initiate output file
 files = os.listdir('output/')
+pp_tags = ''
+for value in p_tags:
+	pp_tags += f' ; {value}'
 
 if output_name[7:] not in files:
-	with open(output_name,'w') as texto:
-		texto.write('Accession code ; Link ; Citation ; Experiment Type ; Platform ; Organism ; Samples ; SRA ; SRA Link ; Tissue ; Cell type ; Cell line ; Title \n')
+	if len(p_tags) > 0:
+		with open(output_name,'w') as texto:
+			texto.write(f'Accession code ; Link ; Citation ; Experiment Type ; Platform ; Organism ; Samples ; SRA ; SRA Link ; Tissue ; Cell type ; Cell line ; Race ; Ethnicity ; Ancestry {pp_tags} ; Title \n')
+	else:
+		with open(output_name,'w') as texto:
+			texto.write(f'Accession code ; Link ; Citation ; Experiment Type ; Platform ; Organism ; Samples ; SRA ; SRA Link ; Tissue ; Cell type ; Cell line ; Race ; Ethnicity ; Ancestry ; Title \n')
+	all_codes = len(codes)
 else:
 	with open(output_name,'r') as texto:
 		done_codes = []
@@ -315,71 +446,95 @@ else:
 				linha = line.split()
 				if len(linha)>0:
 					done_codes.append(linha[0])
-		codes = [value for value in codes if value not in done_codes]		
+		all_codes = len(codes)
+		codes = [value for value in codes if value not in done_codes]
+		n_studies = len(done_codes)
 
 
-#codes = ['GSE48865']
+
+#codes = ['GSE259276','GSE245108','GSE250469','GSE222009','GSE234729']
 	
 
 with open(output_name,'a') as output:		
 	for value in codes:
-	#	if n_studies == 20: #Limits the number of experiments parsed
-	#		break
-		geo_path = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + value
-		html_page = requests.get(geo_path)
-		data_for_studies[value] = {}
-		data_for_studies[value]['Link'] = geo_path
-		if html_page.status_code==200:
-			html_content=html_page.text
-			print('Parsing ' + value + '... \n')
-			with open('tmp/Page.html','w',encoding='utf-8') as file:
-				file.write(html_content)
-			data_for_studies[value]['Citation'] = citationFinder()	
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['Experiment_Type'] =  experimentTyper(texto)
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['Platform'] = platformFinder(texto)
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['Organism'] = organismFinder(texto)
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['Samples'] = sampleFinder(texto)
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['SRA'] = sraChecker(texto)
-				
-			with open('tmp/Page.html','r') as texto:
-				if data_for_studies[value]['SRA'] == 'Yes':
-					data_for_studies[value]['SRA_link'] = sralinkFinder(texto)
+		data_for_studies = {}
+		#if n_studies == 20: #Limits the number of experiments parsed
+		#	break
+		try:
+			geo_path = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + value
+			html_page = requests.get(geo_path)
+			data_for_studies[value] = {}
+			data_for_studies[value]['Link'] = geo_path
+			if html_page.status_code==200:
+				html_content=html_page.text
+				print('Parsing ' + value + '... \n')
+				with open('tmp/Page.html','w',encoding='utf-8') as file:
+					file.write(html_content)
+				data_for_studies[value]['Citation'] = citationFinder()	
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['Experiment_Type'] =  experimentTyper(texto)
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['Platform'] = platformFinder(texto)
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['Organism'] = organismFinder(texto)
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['Samples'] = sampleFinder(texto)
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['SRA'] = sraChecker(texto)
+					
+				with open('tmp/Page.html','r') as texto:
+					if data_for_studies[value]['SRA'] == 'Yes':
+						data_for_studies[value]['SRA_link'] = sralinkFinder(texto)
+					else:
+						data_for_studies[value]['SRA_link'] = 'NA'
+				if complete == True: 
+					data_for_studies[value]['Tissue'], data_for_studies[value]['Cells'], data_for_studies[value]['Lines'], data_for_studies[value]['Race'] , data_for_studies[value]['Ethnicity'] ,  data_for_studies[value]['Ancestry'], personalized = sampledetailFinder()
+					print(personalized)
 				else:
-					data_for_studies[value]['SRA_link'] = 'NA'
-			if complete == True: 
-				data_for_studies[value]['Tissue'], data_for_studies[value]['Cells'], data_for_studies[value]['Lines'] = tissueFinder()
+					data_for_studies[value]['Tissue'] = ''
+					data_for_studies[value]['Cells'] = ''
+					data_for_studies[value]['Lines'] = ''
+					data_for_studies[value]['Race'] = ''
+					data_for_studies[value]['Ethnicity'] = ''
+					data_for_studies[value]['Ancestry'] = ''
+				with open('tmp/Page.html','r') as texto:
+					data_for_studies[value]['Title'] = getTitle(texto)
+				output.write(value + ' ; ')
+				output.write(geo_path + ' ; ')	
+				output.write(data_for_studies[value]['Citation'] + ' ; ')
+				output.write(data_for_studies[value]['Experiment_Type'] + ' ; ')
+				output.write(data_for_studies[value]['Platform'] + ' ; ')
+				output.write(data_for_studies[value]['Organism'] + ' ; ')
+				output.write(data_for_studies[value]['Samples'] + ' ; ')
+				output.write(data_for_studies[value]['SRA'] + ' ; ')
+				output.write(data_for_studies[value]['SRA_link'] + ' ; ')	
+				output.write(data_for_studies[value]['Tissue'] + ' ; ' + data_for_studies[value]['Cells'] + ' ; ' + data_for_studies[value]['Lines'] + ' ; '+ data_for_studies[value]['Race'] + ' ; '+ data_for_studies[value]['Ethnicity'] + ' ; '+ data_for_studies[value]['Ancestry'] + ' ; ')
+				if len(p_tags) > 0:
+					for tag in p_tags:
+						output.write(personalized[tag]['string'] + ' ; ')
+				output.write(data_for_studies[value]['Title'] + '\n')				
+				output.flush()	
+				n_studies += 1		
+				seconds = time() - start
+				tax = (seconds/n_studies*len(codes)) - seconds 
+				print('\nDone (' + str(n_studies) + '/' + str(all_codes)+ ') \n---------\n\n')
 			else:
-				data_for_studies[value]['Tissue'] = ''
-				data_for_studies[value]['Cells'] = ''
-				data_for_studies[value]['Lines'] = ''
-			with open('tmp/Page.html','r') as texto:
-				data_for_studies[value]['Title'] = getTitle(texto)
-			n_studies += 1		
-			seconds = time() - start
-			tax = (seconds/n_studies*len(codes)) - seconds 
-			print('\nDone (' + str(n_studies) + '/' + str(len(codes))+ ') \n---------\n\n')
-		else:
-			print(f'Failed to download HTML. Status code: {html_page.status_code}')
-			output.write(value + ' ; ')
-			output.write(geo_path + ' ; ')	
-			output.write('Connection error \n')
-		output.write(value + ' ; ')
-		output.write(geo_path + ' ; ')	
-		output.write(data_for_studies[value]['Citation'] + ' ; ')
-		output.write(data_for_studies[value]['Experiment_Type'] + ' ; ')
-		output.write(data_for_studies[value]['Platform'] + ' ; ')
-		output.write(data_for_studies[value]['Organism'] + ' ; ')
-		output.write(data_for_studies[value]['Samples'] + ' ; ')
-		output.write(data_for_studies[value]['SRA'] + ' ; ')
-		output.write(data_for_studies[value]['SRA_link'] + ' ; ')	
-		output.write(data_for_studies[value]['Tissue'] + ' ; ' + data_for_studies[value]['Cells'] + ' ; ' + data_for_studies[value]['Lines'] + ' ; ')			
-		output.write(data_for_studies[value]['Title'] + '\n')				
-		output.flush()	
+				print(f'Failed to download HTML. Status code: {html_page.status_code}')
+				error.write(f'{value} {datetime.datetime.now()}\n')
+				error.write(f'{html_page.status_code}\n')
+		except Exception as e:
+			if os.path.exists('output/error_log.txt'):
+				with open('output/error_log.txt','a') as error:
+					error.write(f'{value} {datetime.datetime.now()}\n')
+					error.write(f'{e}\n')
+			else:
+				with open('output/error_log.txt','w') as error:
+					error.write(f'{value} {datetime.datetime.now()}\n')
+					error.write(f'{e}\n')
+			print(f'Error for {value}: {e}')
+			print('Moving to the next study')
+			continue
+				
 	
 	
 
